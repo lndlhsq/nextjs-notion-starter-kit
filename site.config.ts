4851dace97bd6f9d497c19ec49bf4a94c59f30ad
lndlhsq <xmj1370400470@163.com>
2023-07-30 18:03:22 +0800

    更新基础信息

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,24 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '07de7b8964664d70af3762940b949206',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'HiAaBlog',
+  domain: 'https://www.notion.so/HiAaBlog-07de7b8964664d70af3762940b949206?pvs=4',
+  author: 'HaHa',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  weibo: '哈哈的哈士奇',
+  github: 'lndlhsq',
+  公众号: '合于桑林之舞',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
